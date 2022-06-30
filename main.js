1f94ab5be6c61b97192f093a8312ddf56382055b
amaljom <amaljom99@gmail.com>
2022-06-30 16:45:48 +0200

    inserito dinamicamente

--- main.js
+++ main.js
@@ -100,22 +100,25 @@ const posts = [
 const container = document.getElementById('container');
 
 posts.forEach((element, index) =>{
+    if (element.author.image==null || element.author.image=='null'){
+        element.author.image="https://i.pinimg.com/280x280_RS/55/96/4e/55964ebb02710d6b9ce1c26f1d857906.jpg"   
+    }
     container.innerHTML+= `
     <div class="post">
             <div class="post__header">
                 <div class="post-meta">
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
+                        <div class="post-meta__author">${element.author.name}</div>
                         <div class="post-meta__time">4 mesi fa</div>
                     </div>
                 </div>
             </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__text">${element.content}</div>
             <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
+                <img src="${element.media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
@@ -126,7 +129,7 @@ posts.forEach((element, index) =>{
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
                     </div>
                 </div>
             </div>
